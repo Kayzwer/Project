af25cb34699ed79bce113ad866930e2b1172235c
Kayzwer <lkyu753@gmail.com>
2021-12-19 18:36:30 +0800

    make header bold

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ $page }} - {{ config('app.name') }}</title> 
@@ -57,7 +56,6 @@
         {{-- <script src="{{ asset('assets') }}/js/plugins/chartjs.min.js"></script> --}}
         <!--  Notifications Plugin    -->
         <script src="{{ asset('assets') }}/js/plugins/bootstrap-notify.js"></script>
-
         <script src="{{ asset('assets') }}/js/white-dashboard.min.js?v=1.0.0"></script>
         <script src="{{ asset('assets') }}/js/theme.js"></script>
         <script>
